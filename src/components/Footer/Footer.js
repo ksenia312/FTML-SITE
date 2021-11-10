67162797accf7d1898a13bd9241c5168c942cb7c
ksenia312 <ksynikitina.02@mail.ru>
2021-11-10 14:53:16 +0300

    longer blocks

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import style from './Footer.module.scss';
 import Button from "../Button/Button";
-import Vk from '../../assets/files/vk.png'
 
 export default class Footer extends React.Component {
   render() {
     return (
       <footer className={style.footer}>
         <h2 className={style.h2}>Будем рады видеть <br/> тебя в клубе</h2>
-        <Button content={'Вступить в группу, чтобы быть в курсе всех событий'}/>
+        <div className={style.footer_button}>
+          <Button content={'Вступить в группу, чтобы быть в курсе всех событий'}/>
+        </div>
       </footer>
     )
   }
