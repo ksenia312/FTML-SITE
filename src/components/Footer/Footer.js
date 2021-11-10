aa7cef08ba5ac4ca94f7e8efe8fd6bd162fb7a37
ksenia312 <ksynikitina.02@mail.ru>
2021-11-10 21:01:08 +0300

    responsible for >1800px screen

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,7 +6,7 @@ export default class Footer extends React.Component {
   render() {
     return (
       <footer className={style.footer}>
-        <h2 className={style.h2}>Будем рады видеть <br/> тебя в клубе</h2>
+        <h2 className={style.h2}>Будем рады видеть тебя в клубе</h2>
         <div className={style.footer_button}>
           <Button content={'Вступить в группу, чтобы быть в курсе всех событий'}/>
         </div>
